1f0ec6db7258897ac0e428cd878d1e9ce6ea0df9
Marcelo Lacerda <marceloslacerda@gmail.com>
2024-04-23 20:18:18 -0300

    Add repeating tester

---
+++ repeating_tcp_connection/client.py
@@ -0,0 +1,44 @@
+import logging
+import socket
+import sys
+import time
+
+import common
+
+SLEEP_INTERVAL = 0.5
+
+def client_loop(sock, host):
+    common.send_timestamp(sock, host)
+    logging.debug("Waiting for a reply")
+    data = common.receive_message(sock)
+    if not data:
+        logging.error(f"{host} closed before sending a reply")
+    elif len(data) != common.PAYLOAD_LENGTH:
+        logging.error(f"{host} sent an incomplete payload {data}")
+    else:
+        common.parse_msg_to_timestamp(host, data)
+
+def main():
+    if len(sys.argv) > 1:
+        host = sys.argv[1]
+    else:
+        host = common.HOST
+    logging.info("Starting client program")
+    while True:
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                logging.info("Socket obtained")
+                try:
+                    sock.connect((host, common.PORT))
+                except ConnectionRefusedError:
+                    logging.error("Connection refused")
+                    return
+                logging.info(f"Connected to the server {host} on {common.PORT}")
+                client_loop(sock, host)
+        except BrokenPipeError:
+            logging.error("Broken pipe")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=common.LOGLEVEL, format='%(levelname)s %(asctime)s: %(message)s')
+    main()
---
+++ repeating_tcp_connection/server.py
@@ -0,0 +1,55 @@
+import concurrent.futures
+import logging
+import socket
+
+import common
+
+
+def client_thread(conn, addr):
+    with conn:
+        logging.debug(f"Receiving data from {addr}")
+        try:
+            data = common.receive_message(conn)
+        except ConnectionResetError:
+            logging.exception("Connection reset by peer")
+            return
+        if not data:
+            logging.error(f"{addr} closed before sending a message")
+            return
+        elif len(data) != common.PAYLOAD_LENGTH:
+            logging.error(f"{addr} sent an incomplete payload {data}")
+        else:
+            common.parse_msg_to_timestamp(addr, data)
+        logging.debug(f"Sending reply to {addr}")
+        common.send_timestamp(conn, addr)
+    
+
+def run_server():
+    logging.info("Starting in server mode")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        logging.info(f"Binding to port {common.PORT}")
+        s.bind((common.HOST, common.PORT))
+        logging.info("Listening for incoming connections")
+        s.listen(common.MAX_CONN)
+        futures = [None] * common.MAX_CONN
+        with concurrent.futures.ThreadPoolExecutor(max_workers=common.MAX_CONN) as executor:
+            while True:
+                # Continuously accept connections
+                conn, addr = s.accept()
+                logging.debug(f'Connected by {addr}')
+                idx = -1
+                while True:
+                    # Iter over each worker untill a free one can be found to handle the connection
+                    idx += 1
+                    prev_future = futures[idx]
+                    if prev_future is None or prev_future.done():
+                        # Worker slot is free, assign it the connection and break the loop
+                        if prev_future and prev_future.exception():
+                            logging.exception(prev_future.exception())
+                        futures[idx] = executor.submit(client_thread, conn, addr)
+                        break
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=common.LOGLEVEL, format='%(levelname)s %(asctime)s: %(message)s')
+    run_server()
